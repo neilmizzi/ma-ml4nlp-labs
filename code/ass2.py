510d4ca8021edec6501732c62351a1251e9345d0
Neil Mizzi <neilmizzi@github.com>
2020-11-29 21:25:01 +0100

    create script for assignment 2

---
+++ code/ass2.py
@@ -0,0 +1,75 @@
+from ner_ml import NERML
+
+def part1():
+    print("Running Part 1: Token only feature")
+    # Create NER Instance
+    ner = NERML('./data/reuters-train-tab-stripped.en',
+    './data/gold_stripped.conll', load_embeddings=False)
+
+    # Selection of only tokens
+    sel_feats = ['token']
+
+    # Get Vectorised features
+    vec_feats = ner.get_feat_vect(True, sel_feats)
+
+    # Train on all models and get results
+    for model in ['SVM', 'NB', 'LR']:
+        model = ner.create_classifier(vec_feats, 'SVM')
+        predictions = ner.set_predictions(model, sel_feats)
+        ner.get_prediction_summary(predictions)
+
+
+def part2():
+    print("Running Part 2: All features Included")
+    # Create NER Instance
+    ner = NERML('./data/reuters-train-tab-stripped.en',
+    './data/gold_stripped.conll', load_embeddings=False)
+
+    # Selection of all features
+    sel_feats = ['token', 'ChunkLabel', 'POS-Tag', 'PrevToken', 'NextToken', 'FULLCAPS', 'FirstCaps']
+
+    # Get Vectorised features
+    vec_feats = ner.get_feat_vect(True, sel_feats)
+
+    # Train on all models and get results
+    for model in ['SVM', 'NB', 'LR']:
+        model = ner.create_classifier(vec_feats, 'SVM')
+        predictions = ner.set_predictions(model, sel_feats)
+        ner.get_prediction_summary(predictions)
+
+
+def part3():
+    print("Running Part 3: All features + Word Embeddings Included")
+    # Create NER Instance
+    ner = NERML('./data/reuters-train-tab-stripped.en',
+    './data/gold_stripped.conll', load_embeddings=True)
+
+    # Selection of all features
+    sel_feats = ['token', 'ChunkLabel', 'POS-Tag', 'PrevToken', 'NextToken', 'FULLCAPS', 'FirstCaps']
+
+    # Get Vectorised features
+    vec_feats = ner.get_feat_vect(True, sel_feats)
+
+    # Train on all models and get results
+    for model in ['SVM', 'NB', 'LR']:
+        model = ner.create_classifier(vec_feats, 'SVM')
+        predictions = ner.set_predictions(model, sel_feats)
+        ner.get_prediction_summary(predictions)
+
+
+# Running Assignment 2 Stuff
+if __name__ == "__main__":
+    part_to_run = 1
+    
+    if part_to_run == 1:
+        # Part 1
+        # All Models, without new features or embeddings
+        part1()
+    elif part_to_run == 2:
+        # Part 2
+        # Model without embeddings, but with added features
+        part2()
+    elif part_to_run == 3:
+        # Part 3
+        # Embeddings also included
+        part3()
